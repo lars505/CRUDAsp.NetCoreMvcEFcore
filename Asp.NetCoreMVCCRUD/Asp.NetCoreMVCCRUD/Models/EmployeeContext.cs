d4e4914c962e182c3d998f01cd9c9e8d61fd05f0
Angel Lars <lars50593@gmail.com>
2020-05-18 14:02:18 -0600

    commit3

---
+++ Asp.NetCoreMVCCRUD/Asp.NetCoreMVCCRUD/Models/EmployeeContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreMVCCRUD.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options):base(options)
+        {
+                
+        }
+    }
+}
